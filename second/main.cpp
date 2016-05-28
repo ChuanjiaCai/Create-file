a1c9fc49754f34b5c909574c92a3777bcbf5a992
jack <jack@jackdemacbook-air.local>
2016-05-28 15:28:27 +0800

    1,add reading doc file function.

--- second/main.cpp
+++ second/main.cpp
@@ -4,12 +4,48 @@
 //
 //  Created by jack on 16/5/28.
 //  Copyright © 2016年 jack. All rights reserved.
+//   creates a new file
 //
+//#include <iostream>
+//#include<fstream>
+//using namespace std;
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    ofstream  myfile("example.doc");
+//    if(myfile.is_open())
+//    {
+//        myfile<<"this is a line.\n";
+//        myfile<<"this is another line.\n";
+//        myfile.close();
+//        
+//    }
+//    else
+//        cout<<"unable to open file";
+//    
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//reading a doc file
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+    string line;
+    ifstream myfile("example.doc");
+    if(myfile.is_open())
+    {
+        while(getline(myfile,line))
+        {
+            cout<<line<<'\n';
+            
+        }
+        myfile.close();
+    }
+    else
+        cout<<"unable to open file";
     return 0;
 }
